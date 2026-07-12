7a8dd29ad79cbcd94d86fc98e8c2a568309205e1
Sai Ganesh Chamarty <123051270+SaiChamarty@users.noreply.github.com>
2026-07-12 19:23:53 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,40 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        result = []
+        num_words = len(strs)
+        result.append(str(num_words) + "#")
+        for item in strs:
+            length = str(len(item))
+            result.append(length)
+            result.append("#")
+            result.append(item)
+        answer = "".join(result)
+        return answer
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        current_str = []
+        index = 0
+        while s[index] != "#":
+            index += 1
+        num_words = int(s[0:index])
+        print(num_words)
+        print(s)
+        index+=1
+        for i in range(num_words):
+            temp = index
+            separator = s[temp]
+            while separator != "#":
+                temp += 1
+                separator = s[temp]
+            length_of_word = int(s[index:temp])
+            index = temp + 1
+            current_str.append(s[index:index+length_of_word])
+            index += length_of_word
+            result.append("".join(current_str))
+            current_str = []
+            
+        return result
+        
+           
